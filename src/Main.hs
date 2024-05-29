b9b3c5c3976155d3c7e2210f54baf659cf0d04db
David Young <roboguy06@gmail.com>
2024-05-29 14:09:57 -0500

    profiling

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,16 @@
 module Main where
 
+import Test.Tasty
+
+import Test.QuickCheck
+
+import QSym.Monad
+import QSym.Interpret
+import QSym.Syntax
+
+import QSym.Tests.Arithmetic
+
 main :: IO ()
-main = error "QSym.main"
+main =
+  mapM_ quickCheck [checkAdder]
 
