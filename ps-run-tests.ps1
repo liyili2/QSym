13e50cdc1773230204ae758bea570e7e7a6afe67
lochnessdragon <39635811+lochnessdragon@users.noreply.github.com>
2024-06-13 17:10:32 -0500

    Added powershell run tests script
    ps- namespace chosen to avoid collision with run-tests.sh, allowing others to quickly and easily use tab completion to run that file.

---
+++ ps-run-tests.ps1
@@ -0,0 +1,5 @@
+# Don't forget to run: "Set-ExecutionPolicy RemoteSigned" on your local machine.
+# see: https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_execution_policies?view=powershell-7.4
+# and: https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_scripts?view=powershell-7.4
+
+cabal test --test-show-detail=streaming --enable-profiling --profiling-detail=all-functions @Args
