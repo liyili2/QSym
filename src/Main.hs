605641bb5887560eb76d903e796f862936b91753
David Young <roboguy06@gmail.com>
2024-06-23 22:22:39 -0500

    parse BellPair test

--- src/Main.hs
+++ src/Main.hs
@@ -10,8 +10,12 @@ import QSym.Syntax
 
 import QSym.Tests.Arithmetic
 
+import Qafny.Syntax.Parser
+
 main :: IO ()
-main =
+main = do
+  (print . scanAndParse) =<< readFile "tests/BellPair.qfy"
+
   mapM_ quickCheck
     [ checkrzAdder
     -- , checkAdder
