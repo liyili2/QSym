13673bd48a3e631a0de50544a988c76f97d2c7db
David Young <roboguy06@gmail.com>
2024-06-29 00:49:47 -0500

    Step-by-step example of how a circuit diagram can be thought of as being created from code

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+
 module Main where
 
 import Test.Tasty
@@ -11,6 +13,40 @@ import QSym.Syntax
 import QSym.Tests.Arithmetic
 
 import Qafny.Syntax.Parser
+import Qafny.Syntax.AST
+import Qafny.TTG
+
+import Data.Sum
+
+import qualified QSym.Syntax as QSym
+
+-- Bell pair:
+--
+--
+-- q[0] ---> [H] --+------->
+--                 |
+--                 |
+--                 v
+-- q[1] --------> (+) ----->
+
+
+-- TODO: Support for non-unit lists
+interpretQafny :: AST -> QSym.Expr
+interpretQafny [Toplevel (Inl x)] =
+  let Just body = qmBody x
+  in
+  interpretBlock body
+
+interpretBlock :: Block () -> QSym.Expr
+interpretBlock (Block xs) = foldr1 QSym.Seq (map interpretStmt xs)
+
+interpretStmt :: Stmt () -> QSym.Expr
+interpretStmt (x :*=: y) = undefined
+interpretStmt (SIf cond partition block) = undefined
+
+interpretExpr :: Exp () -> QSym.Expr
+interpretExpr EHad = undefined
+interpretExpr (EOp2 OAdd x y) = undefined
 
 main :: IO ()
 main = do
