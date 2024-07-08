f63a5530df5f47160cb47c4388a683c35c19985f
David Young <roboguy06@gmail.com>
2024-07-08 15:58:50 -0500

    constraints

--- src/QSym/Logic/Syntax.hs
+++ src/QSym/Logic/Syntax.hs
@@ -6,31 +6,32 @@ import Prettyprinter
 data Range =
   Range
   { rangeName :: String
-  , rangeStart :: Int
-  , rangeEnd :: Int
+  , rangeStart :: SimpleExpr
+  , rangeEnd :: SimpleExpr
   }
-  deriving (Show)
+  deriving (Show, Eq)
 
 data SimpleExpr
   = LocusVar SteppedLocus
   | Var String -- | "Logical" variable
   | Xor SimpleExpr SimpleExpr
+  | Add SimpleExpr SimpleExpr
   | Lit Int
   | Hadamard String
-  deriving (Show)
+  deriving (Show, Eq)
 
 -- | Invariant: Ranges in list should be disjoint
 newtype Locus = Locus [Range]
-  deriving (Show)
+  deriving (Show, Eq)
 
 -- | Keeps track of the time step
 data Stepped a = Current a | Step (Stepped a)
-  deriving (Show)
+  deriving (Show, Eq)
 
 type SteppedLocus = Stepped Locus
 
-data Prop a = Prop [Conjunct a]
-  deriving (Show)
+newtype Prop a = Prop [Conjunct a]
+  deriving (Show, Semigroup, Monoid)
 
 data Conjunct a
   = PointsTo SteppedLocus a
@@ -51,7 +52,7 @@ type LExprConjunct = Conjunct LExpr
 
 instance Pretty Range where
   pretty r
-    | rangeEnd r == rangeStart r + 1 = pretty (rangeName r) <> pretty "[" <> pretty (rangeStart r) <> pretty "]"
+    | rangeEnd r == (rangeStart r `Add` Lit 1) = pretty (rangeName r) <> pretty "[" <> pretty (rangeStart r) <> pretty "]"
     | otherwise =
         pretty (rangeName r) <> pretty "[" <> pretty (rangeStart r) <> pretty "," <+> pretty (rangeEnd r) <> pretty ")"
 
@@ -93,10 +94,10 @@ instance Pretty SimpleExpr where
 instance Pretty a => Pretty (Lambda a) where
   pretty (x :=> body) = parens $ pretty x <+> pretty "=>" <+> pretty body
 
-unitRange :: String -> Int -> Range
-unitRange x i = Range x i (i+1)
+unitRange :: String -> SimpleExpr -> Range
+unitRange x i = Range x i (Add i (Lit 1))
 
-unitLocus :: String -> Int -> Locus
+unitLocus :: String -> SimpleExpr -> Locus
 unitLocus x i = Locus [unitRange x i]
 
 separatedBy :: Doc a -> [Doc a] -> Doc a
@@ -104,22 +105,53 @@ separatedBy separator xs = concatWith go xs
   where
     go a b = a <+> separator <+> b
 
+class Steppable a where
+  step :: a -> a
+
+instance Steppable (Stepped a) where
+  step = Step
+
+instance Steppable a => Steppable (Prop a) where
+  step (Prop xs) = Prop $ map step xs
+
+instance Steppable a => Steppable (Conjunct a) where
+  step (PointsTo lhs rhs) = PointsTo (step lhs) (step rhs)
+  step (Unchanged x) = Unchanged (step x)
+
+instance Steppable SimpleExpr where
+  step (Xor x y) = Xor (step x) (step y)
+  step (Add x y) = Add (step x) (step y)
+  step (LocusVar x) = LocusVar $ step x
+  step x = x -- NOTE: This might need to be extended if we add more constructors to SimpleExpr
+
+instance Steppable a => Steppable (Lambda a) where
+  step (x :=> body) = x :=> step body
+
+instance Steppable LExpr where
+  step (LApply x rhs) =
+    LApply (step x) (step rhs)
+
+  step (LIfThenElse c t f) =
+    LIfThenElse (step c) (step t) (step f)
+
+  step (LSimpleExpr e) = LSimpleExpr $ step e
+
 testProp :: LExprProp
 testProp =
   Prop
     [ -- Step 0
-      PointsTo (Current (unitLocus "q" 0)) (LSimpleExpr (Var "a"))
-    , PointsTo (Current (unitLocus "q" 1)) (LSimpleExpr (Var "b"))
+      PointsTo (Current (unitLocus "q" (Lit 0))) (LSimpleExpr (Var "a"))
+    , PointsTo (Current (unitLocus "q" (Lit 1))) (LSimpleExpr (Var "b"))
 
       -- Step 1
-    , PointsTo (Step (Current (unitLocus "q" 0))) (LApply (Current (unitLocus "q" 0)) ("x" :=> Hadamard "x"))
-    , Unchanged (Step (Current (unitLocus "q" 1)))
+    , PointsTo (Step (Current (unitLocus "q" (Lit 0)))) (LApply (Current (unitLocus "q" (Lit 0))) ("x" :=> Hadamard "x"))
+    , Unchanged (Step (Current (unitLocus "q" (Lit 1))))
 
       -- Step 2
-    , Unchanged (Step (Step (Current (unitLocus "q" 0))))
-    , PointsTo (Step (Step (Current (unitLocus "q" 1))))
-        $ LIfThenElse (LocusVar (Step (Current (unitLocus "q" 0))))
-            (LApply (Step (Current (unitLocus "q" 1))) ("x" :=> Xor (Var "x") (Lit 1)))
-            (LocusVar (Step (Current (unitLocus "q" 1))))
+    , Unchanged (Step (Step (Current (unitLocus "q" (Lit 0)))))
+    , PointsTo (Step (Step (Current (unitLocus "q" (Lit 1)))))
+        $ LIfThenElse (LocusVar (Step (Current (unitLocus "q" (Lit 0)))))
+            (LApply (Step (Current (unitLocus "q" (Lit 1)))) ("x" :=> Xor (Var "x") (Lit 1)))
+            (LocusVar (Step (Current (unitLocus "q" (Lit 1)))))
     ]
 
