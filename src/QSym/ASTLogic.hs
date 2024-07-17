bcd5b6d7b703ac9d3267bf5bf99a0f3dd6bc682c
FreeLevistus <borgstadtmt@gmail.com>
2024-07-17 15:01:31 -0500

    Added ArithExpr and BoolExpr

--- src/QSym/ASTLogic.hs
+++ src/QSym/ASTLogic.hs
@@ -6,16 +6,13 @@ import QSym.Utils
 import QSym.Logic.Syntax
 import QSym.Tests.Arithmetic
 
---Is a normal Var with the addition of an address field to keep track of the Locus it represents
-data PredVar = {value :: Var, address :: Locus}
-
 type Sum = Nat Nat [Complex] [Natural]
 
 data PredElem = Addr Var | Heap Var | Val QState | V Sum deriving (Eq, Show)
 
 --This is the core type of the predicates respresenting the symbolic executor at each step
 data Pred = 
-    Equal PredElem PredElem |
+    Equal PredElem PredElem | 
     Less PredElem PredElem |
     Not Pred | 
     And Pred Pred |     
@@ -25,14 +22,23 @@ data Pred =
 
 -- Arithmetic Expression, Comes from page 10 figure 7 in https://arxiv.org/pdf/2211.06411
 data ArithExpr = 
+    ArithVar Var |              -- Normal Variable
+    ArithRange Var Nat Nat |    -- Range of bits in a variable
+    Scal Real |                 -- Scalar value
+    Add ArithExpr ArithExpr |
+    Mult ArithExpr ArithExpr
 
 -- Boolean Expression, Comes from page 10 figure 7 in https://arxiv.org/pdf/2211.06411
 data BoolExpr =
+    Bit Var Nat |   -- Gets boolean value from a bit
+    CEq Equal Bit | -- Checks for equality using a control bit
+    CLess Less Bit  -- Compares values using a control bit
+
 
 data QafnyExpr =
-    Apply Locus ArithExpr | -- Used to apply arithmetic operations on locus in the interperet function
-    IfExp BoolExpr QafnyExpr | -- If BoolExpr, then QanfyExpr. Used in interepret function
-    Measure Locus -- Placeholder for now
+    Apply Locus ArithExpr |     -- Used to apply arithmetic operations on locus in the interperet function
+    IfExp BoolExpr QafnyExpr |  -- If BoolExpr, then QanfyExpr. Used in interepret function
+    Measure Locus               -- Placeholder for now
 
 interpret :: LMap -> Pred -> QafnyExpr -> Pred
 interpret lm p (Apply loc expr) = And p (And (Equal (Addr x)) (Addr new)) (Equal (Heap new) (Apply e (Heap x)))
