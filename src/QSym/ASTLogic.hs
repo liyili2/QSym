65a1b418fb2f8b8ca8c386e005a73aceab93f343
FreeLevistus <borgstadtmt@gmail.com>
2024-07-18 13:32:23 -0500

    changes from meeting

--- src/QSym/ASTLogic.hs
+++ src/QSym/ASTLogic.hs
@@ -1,4 +1,4 @@
-module Qdym.ASTLogic
+module QSym.ASTLogic
     where
 
 import QSym.Syntax
@@ -11,9 +11,9 @@ import QSym.Tests.Arithmetic
 -- Map of loci and corresponding vars. When a new var is created, it gets added to the front of the corresponding locus's list
 type LMap = [(Locus, [Var])]
 
-type Sum = Nat Nat [Complex] [Natural] 
+type Sum = Nat Nat [Complex] [Natural]
 
-data PredElem = Addr Var | Heap Var | Val QState | V Sum deriving (Eq, Show) -- CLARIFY: Talked briefly last time about maybe changing Addr and Heap to Locus type
+data PredElem = Addr Var | Heap Var | Val QState | V Sum deriving (Eq, Show)
 
 -- This is the core type of the predicates respresenting the symbolic executor at each step
 data Pred = 
@@ -36,9 +36,9 @@ data ArithExpr =
 
 -- Boolean Expression, Comes from page 10 figure 7 in https://arxiv.org/pdf/2211.06411
 data BoolExpr =
-    Bit Var Nat |   -- Gets boolean value from a bit
-    CEq Equal Bit | -- Checks for equality using a control bit | CLARIFY: Not sure if this should be using the Pred Equal or not
-    CLess Less Bit  -- Compares values using a control bit
+    Bit Var ArithExpr |   -- Gets boolean value from a bit
+    CEq ArithExpr ArithExpr Var ArithExpr | -- Checks for equality using a control bit
+    CLess ArithExpr ArithExpr Var ArithExpr  -- Compares values using a control bit
     deriving (Eq, Show, Ord)
 
 data QafnyExpr =
@@ -47,14 +47,13 @@ data QafnyExpr =
     Measure Locus               -- Placeholder for now
 
 -- Finds the current (first index) variable for a given Locus
-lookup :: LMap -> Locus -> Var
-lookup x:xs loc
-    | (fst x) == loc || length xs == 0 = first . lst x -- TODO: Right now it assumes the last element if it gets there, ideally throws an error with an empty list instead
-    | otherwise = lookup xs loc
+lookup :: LMap -> Locus -> Maybe Var
+lookup [] _ = Nothing
+lockup (x:xs) loc = if (fst x == loc) then Just (snd x) else lookup xs loc
 
 -- Returns the new predicate from applying the QafnyExpr to the old Pred
-interpret :: LMap -> Pred -> QafnyExpr -> Pred
-interpret lm p (Apply loc expr) = And p (And (Equal (Addr x) (Addr new)) (Equal (Heap new) (Apply expr (Heap x)))) -- CLARIFY: apply expr (Heap x) should be a PredElem, so either PredElem needs new definition or apply needs to be a function
---interpret lm p (IfExp be qe) = CLARIFY: Not really sure what this should like like, just that something else in this file has to be changed
+interpret :: LMap -> Pred -> QafnyExpr -> (Pred, LMap)
+interpret lm p (Apply loc expr) = (And p (And (Equal (Addr x) (Addr new)) (Equal (Heap new) (Apply expr (Heap x)))), addVar lm new)
+--interpret lm p (IfExp be qe) = 
     where x = lookup lm loc
-          new = newVar lm loc x -- CLARIFY: Not sure how this works without figuring out how the LMap is tracked first
+          new = newVar lm loc x
