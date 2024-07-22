58433b2627c87927198fa5c8bcfa802aab9e1787
FreeLevistus <borgstadtmt@gmail.com>
2024-07-22 16:54:44 -0500

    a few more implementations in ASTLogic

--- src/QSym/ASTLogic.hs
+++ src/QSym/ASTLogic.hs
@@ -20,16 +20,6 @@ type Sum = Nat Nat [Complex] [Natural]
 
 data PredElem = Addr Var | Heap Var | Val QState | V Sum deriving (Eq, Show)
 
--- This is the core type of the predicates respresenting the symbolic executor at each step
-data Pred = 
-    Equal PredElem PredElem | 
-    Less PredElem PredElem |
-    Not Pred | 
-    And Pred Pred |     
-    Or Pred Pred |      
-    Imply Pred Pred
-    deriving (Eq, Show, Ord)
-
 -- Arithmetic Expression, Comes from page 10 figure 7 in https://arxiv.org/pdf/2211.06411
 data ArithExpr = 
     ArithVar Var |              -- Normal Variable
@@ -46,6 +36,17 @@ data BoolExpr =
     CLess ArithExpr ArithExpr Var ArithExpr  -- Compares values using a control bit
     deriving (Eq, Show, Ord)
 
+-- This is the core type of the predicates respresenting the symbolic executor at each step
+data Pred = 
+    Equal PredElem PredElem | 
+    Less PredElem PredElem |
+    Not Pred | 
+    And Pred Pred |     
+    Or Pred Pred |      
+    Imply Pred Pred |
+    BEBit BoolExpr
+    deriving (Eq, Show, Ord)
+
 data QafnyExpr =
     Apply Locus ArithExpr |     -- Used to apply arithmetic operations on locus in the interperet function
     IfExp BoolExpr QafnyExpr |  -- If BoolExpr, then QanfyExpr. Used in interepret function
@@ -54,11 +55,17 @@ data QafnyExpr =
 -- Finds the current (first index) variable for a given Locus
 lookup :: LMap -> Locus -> Maybe Var
 lookup [] _ = Nothing
-lockup (x:xs) loc = if (fst x == loc) then Just (snd x) else lookup xs loc
+lockup (x:xs) loc = if (fst x == loc) then Just (first . snd x) else lookup xs loc
+
+-- Inserts the new var at the front of the list in the target locus's tuple
+-- If the target locus can't be found, creates a new entry tuple at the end of the LMap
+addVar :: LMap -> Locus -> Var -> LMap
+lookup [] loc v = [(loc, [v])]
+lookup (x:xs) loc v = if (fst x == loc) then (loc, v:(snd x)):(lookup xs loc v) else x:(lookup xs loc v)
 
 -- Returns the new predicate from applying the QafnyExpr to the old Pred
 interpret :: LMap -> Pred -> QafnyExpr -> (Pred, LMap)
-interpret lm p (Apply loc expr) = (And p (And (Equal (Addr x) (Addr new)) (Equal (Heap new) (Apply expr (Heap x)))), addVar lm new)
---interpret lm p (IfExp be qe) = 
+interpret lm p (Apply loc expr) = (And p (And (Equal (Addr x) (Addr new)) (Equal (Heap new) (arithApply expr (Heap x)))), addVar lm new)
+interpret lm p IfExp be qe = (Or (And (Not (BEBit be)) p) (And (BEBit be) (interpret lm p qe)), lm)
     where x = lookup lm loc
           new = newVar lm loc x
