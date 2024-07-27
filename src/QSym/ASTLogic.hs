a0f67e6aa8693bbb0087e4d5ad5af9f846339bfb
FreeLevistus <borgstadtmt@gmail.com>
2024-07-26 22:30:23 -0500

    Fixed predicate structure in ASTLogic and fixed the arithApply function

--- src/QSym/ASTLogic.hs
+++ src/QSym/ASTLogic.hs
@@ -1,71 +1,91 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# OPTIONS_GHC -Wno-missing-signatures #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Redundant bracket" #-}
 module QSym.ASTLogic
     where
 
 import Prelude hiding (lookup)
-import qualified Prelude
-
-import QSym.Syntax
-import QSym.QState
+import Numeric.Natural
+import Data.Maybe
 import QSym.Monad
 import QSym.Utils
-import QSym.Logic.Syntax
-
-import Numeric
-import Data.Complex
+import QSym.Logic.Syntax hiding (Add)
 
 -- Map of loci and corresponding vars. When a new var is created, it gets added to the front of the corresponding locus's list
 type LMap = [(Locus, [Var])]
 
-type Sum = Nat Nat [Complex] [Natural]
-
-data PredElem = Addr Var | Heap Var | Val QState | V Sum deriving (Eq, Show)
+data Complex = Complex Double Double
+data Sum = Sum Natural Natural [Complex] [Natural]
 
 -- Arithmetic Expression, Comes from page 10 figure 7 in https://arxiv.org/pdf/2211.06411
-data ArithExpr = 
-    ArithVar Var |              -- Normal Variable
-    ArithRange Var Nat Nat |    -- Range of bits in a variable
-    Scal Real |                 -- Scalar value
+data ArithExpr =
+    AddrVar Var |
+    HeapVar Var |
+    StateVal QState' |
+    SumVal Sum |
+    ArithRange Var Natural Natural |    -- Range of bits in a variable
+    Scal Double |                         -- Scalar value
     Add ArithExpr ArithExpr |
-    Mult ArithExpr ArithExpr
-    deriving (Eq, Show, Ord)
+    Sub ArithExpr ArithExpr |
+    Mult ArithExpr ArithExpr |
+    Div ArithExpr ArithExpr
+    --deriving (Eq, Show, Ord)
 
 -- Boolean Expression, Comes from page 10 figure 7 in https://arxiv.org/pdf/2211.06411
 data BoolExpr =
     Bit Var ArithExpr |   -- Gets boolean value from a bit
     CEq ArithExpr ArithExpr Var ArithExpr | -- Checks for equality using a control bit
     CLess ArithExpr ArithExpr Var ArithExpr  -- Compares values using a control bit
-    deriving (Eq, Show, Ord)
+    --deriving (Eq, Show, Ord)
 
 -- This is the core type of the predicates respresenting the symbolic executor at each step
-data Pred = 
-    Equal PredElem PredElem | 
-    Less PredElem PredElem |
-    Not Pred | 
-    And Pred Pred |     
-    Or Pred Pred |      
+data Pred =
+    Equal ArithExpr ArithExpr |
+    Less ArithExpr ArithExpr |
+    Not Pred |
+    And Pred Pred |
+    Or Pred Pred |
     Imply Pred Pred |
     BEBit BoolExpr
-    deriving (Eq, Show, Ord)
+    --deriving (Eq, Show, Ord)
 
 data QafnyExpr =
     Apply Locus ArithExpr |     -- Used to apply arithmetic operations on locus in the interperet function
     IfExp BoolExpr QafnyExpr |  -- If BoolExpr, then QanfyExpr. Used in interepret function
     Measure Locus               -- Placeholder for now
 
+newVar :: LMap -> Locus -> Var -> (LMap, Var) -- Placeholder until monad solution is implemented
+newVar lm _ x = (lm, x)
+
 -- Finds the current (first index) variable for a given Locus
 lookup :: LMap -> Locus -> Maybe Var
 lookup [] _ = Nothing
-lockup (x:xs) loc = if (fst x == loc) then Just (first . snd x) else lookup xs loc
+lockup (x:xs) loc = if fst x == loc then Just (head (snd x)) else lookup xs loc
 
 -- Inserts the new var at the front of the list in the target locus's tuple
 -- If the target locus can't be found, creates a new entry tuple at the end of the LMap
 addVar :: LMap -> Locus -> Var -> LMap
-lookup [] loc v = [(loc, [v])]
-lookup (x:xs) loc v = if (fst x == loc) then (loc, v:(snd x)):(lookup xs loc v) else x:(lookup xs loc v)
+addVar [] loc v = [(loc, [v])]
+addVar (x:xs) loc v = if fst x == loc then (loc, v:(snd x)):(addVar xs loc v) else x:(addVar xs loc v)
 
 -- Returns the new predicate from applying the QafnyExpr to the old Pred
 interpret :: LMap -> Pred -> QafnyExpr -> (Pred, LMap)
-interpret lm p (Apply loc expr) = (And p (And (Equal (Addr x) (Addr new)) (Equal (Heap new) (arithApply expr (Heap x)))), addVar lm new)
-interpret lm p IfExp be qe = (Or (And (Not (BEBit be)) p) (And (BEBit be) (interpret lm p qe)), lm)
-    where x = lookup lm loc
-          new = newVar lm loc x
+interpret lm p (IfExp be qe) = (Or (And (Not (BEBit be)) p) (And (BEBit be) (fst prevPred)), snd prevPred)
+    where prevPred = interpret lm p qe
+interpret lmap p (Apply loc expr) = (And p (And (Equal (AddrVar x) (AddrVar new)) (Equal (HeapVar new) (arithApply expr (HeapVar x)))), addVar lm loc new)
+    where x = fromJust (lookup lmap loc)
+          newTuple = newVar lmap loc x
+          lm = fst newTuple
+          new = snd newTuple
+
+-- Takes an ArithExpr and applies it on another ArithExpr
+-- Wherever a var appears in the first ArithExpr, the second ArithExpr is inserted
+arithApply :: ArithExpr -> ArithExpr -> ArithExpr
+arithApply (HeapVar _) x = x        -- Base case that replaces the placeholder var with the supplied expression
+arithApply (AddrVar _) x = x        -- Base case that replaces the placeholder var with the supplied expression
+arithApply (Scal n) _ = Scal n      -- Base case that just returns a scalar back to its parent expression
+arithApply (Add expr1 expr2) x = Add (arithApply expr1 x) (arithApply expr2 x)
+arithApply (Sub expr1 expr2) x = Sub (arithApply expr1 x) (arithApply expr2 x)
+arithApply (Mult expr1 expr2) x = Mult (arithApply expr1 x) (arithApply expr2 x)
+arithApply (Div expr1 expr2) x = Div (arithApply expr1 x) (arithApply expr2 x)
